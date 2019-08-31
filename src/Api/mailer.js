34287e98ce079ff2eb1720d1e91dc9a3c30b52a3
Ryan Callahan <RyanCallahan312@gmail.com>
2019-08-31 10:48:46 -0400

    update

--- src/Api/mailer.js
+++ src/Api/mailer.js
@@ -1,10 +1,9 @@
-import mailerInfo from '../Constants/mailerInfo.json';
-import axios from 'axios';
+import mailerInfo from "../Constants/mailerInfo.json";
+import axios from "axios";
 
 export async function sendMail(mailData) {
-	const form = await axios.post('/api/mailer', {
-		mailData,
-		mailerInfo
-    });
-    console.log(form, 'here')
+  axios.post("api/mailer", {
+    mailData,
+    mailerInfo
+  });
 }
