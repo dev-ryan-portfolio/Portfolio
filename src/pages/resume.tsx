d6f7fea869bed8fae4352cae9f0c65ec65b308ca
Ryan Callahan <RyanCallahan312@gmail.com>
2022-01-17 23:26:44 -0800

    update resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,42 +1,19 @@
 import * as React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import { GatsbyImage, getImage, IGatsbyImageData } from 'gatsby-plugin-image';
 
 import Layout from '@components/common/defaultLayout';
 import SEO from '@components/common/seo';
 import '@styles/resume.css';
-interface ResumeImage {
-	allFile: {
-		nodes: [{ childImageSharp: IGatsbyImageData; base: string }];
-	};
-}
 
 const Resume: React.FC = () => {
-	const data: ResumeImage = useStaticQuery(graphql`
-    {
-        allFile(
-        filter: {relativeDirectory: {eq: "resume"}, base: {eq: "Resume.jpg"}}
-        ) {
-            nodes {
-                childImageSharp {
-                    gatsbyImageData(
-                        placeholder: BLURRED
-                        formats: [AUTO, WEBP, AVIF]
-                    )
-                }
-                base
-            }
-        }
-    }
-    `);
 	return (
 		<Layout>
 			<SEO title='Resume' />
-			<GatsbyImage
-				image={getImage(data.allFile.nodes[0].childImageSharp)}
-				className='resume-img'
-				alt='my resume'
-			/>
+			<div className='resume-container'>
+				<iframe
+					src='https://drive.google.com/file/d/1KX1R2xiUxTOGP0--Sk69GoNK3EfBf5xO/preview'
+					className='resume-img'
+					allow='autoplay'></iframe>
+			</div>
 		</Layout>
 	);
 };
