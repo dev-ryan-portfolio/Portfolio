6f31af3149ab9387aa919ca86ed43f80822a1308
Ryan Callahan <RyanCallahan312@gmail.com>
2022-01-23 19:48:55 -0800

    Updates

---
+++ component---src-pages-resume-tsx-2a5c82d2a36154347ea4.js
@@ -0,0 +1,2 @@
+(self.webpackChunkdev_ryan_portfolio=self.webpackChunkdev_ryan_portfolio||[]).push([[753],{5225:function(e,t,r){"use strict";r.r(t);var a=r(7294),l=r(3609),n=r(8362);t.default=function(){return a.createElement(l.Z,null,a.createElement(n.Z,{title:"Resume"}),a.createElement("div",{className:"resume-container"},a.createElement("iframe",{src:"https://drive.google.com/file/d/1S5hmxrJTeMhR4FDXGyCfCOmVCuzMEJf-/preview",className:"resume-img",allow:"autoplay"})))}}}]);
+//# sourceMappingURL=component---src-pages-resume-tsx-2a5c82d2a36154347ea4.js.map
